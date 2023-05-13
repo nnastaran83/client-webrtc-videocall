c607f1ed1406a685d1e74208c8e38a41af0896e5
nnastaran83 <nnastaran83@gmail.com>
2023-05-13 18:52:48 +0300

    host

--- src/firebase_module/index.js
+++ src/firebase_module/index.js
@@ -16,16 +16,17 @@ import { getMessaging, getToken } from "firebase/messaging";
  * @see https://firebase.google.com/docs/web/setup#available-libraries
  * @type {{storageBucket: string, apiKey: string, messagingSenderId: string, appId: string, projectId: string, measurementId: string, authDomain: string}}
  */
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBWLr7DrODHLbB2Zvb5o6ANvBAws0MH8_8",
   authDomain: "smart-4e774.firebaseapp.com",
   projectId: "smart-4e774",
   storageBucket: "smart-4e774.appspot.com",
   messagingSenderId: "267084890357",
-  appId: "1:267084890357:web:6aa5d0b170c3dcb44f45bc",
-  measurementId: "G-KQTK3EWGST",
+  appId: "1:267084890357:web:73c058772bfe998b4f45bc",
+  measurementId: "G-XLLC8D755N",
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
