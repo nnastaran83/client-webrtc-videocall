830920b2718656429a44ddf9e019b7a62afafc27
nnastaran83 <nnastaran83@gmail.com>
2023-06-18 20:01:05 +0300

    ss

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     server: {
@@ -11,6 +12,5 @@ export default defineConfig({
         strictPort: true,
         port: 8000, // you can replace this port with any port
     },
-
+    publicDir: "public",
 });
-
